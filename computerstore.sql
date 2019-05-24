800b69ee218b6306cec2df9947ac13db87fe4654
osamaalzahabi <50995895+osamaalzahabi@users.noreply.github.com>
2019-05-24 19:19:09 +0300

    Update computerstore.sql

--- computerstore.sql
+++ computerstore.sql
@@ -1,1 +1,74 @@
 
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2019 at 06:17 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `computerstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `computers`
+--
+
+CREATE TABLE `computers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` varchar(1000) NOT NULL,
+  `cpu` varchar(1000) NOT NULL,
+  `screen` varchar(100) NOT NULL,
+  `ram` varchar(100) NOT NULL,
+  `storage` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `computers`
+--
+
+INSERT INTO `computers` (`id`, `name`, `price`, `cpu`, `screen`, `ram`, `storage`) VALUES
+(11, 'HP Spectre x360', '949$', 'Intel Core i5', '13.3-inch full HD', '8GB', '1TB'),
+(12, 'HP Spectre 13', '939$', '8th Gen Intel Core i5', '13.3-inch, FHD (1,920 x 1,080)', '16GB', '256GB'),
+(13, 'MacBook', '3000$', '1.2GHz', 'Retina display', '16GB', '256GB');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `computers`
+--
+ALTER TABLE `computers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `computers`
+--
+ALTER TABLE `computers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
